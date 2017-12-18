627efd1fa06ad0257789015ba2a7c08d7663b363
Sebastian <seba_an14@hotmail.com>
2017-12-17 21:09:06 -0300

    funcion limpiar consultar y cambios bd
    agregue la tabla periodo a la bd
    agregue la funcion limpiar combobox y textbox
    agregue la funcion consultar pero solo para los primeros 2 periodos del dia lunes 
    la consulta se permite si ambos combobox estan seleccionados o si el combobox de laboratorio esta seleccionado por si solo

---
+++ SIA-master/appSIA/utemAppWF/Profesor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utemAppWF
+{
+    public class Profesor
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreCompleto
+        {
+            get
+            {
+                return $"{Nombre} {Apellido}";
+            }
+        }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+    }
+}
