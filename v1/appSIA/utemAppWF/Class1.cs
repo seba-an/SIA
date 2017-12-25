50ba4ff2ad5d103d3badfad6cae2b0b2f00be726
Sebastian <seba_an14@hotmail.com>
2017-12-25 19:04:10 -0300

    v1 actualizado
    me pitie la consulta pero lo arreglo mañana. 
    vean el tema de add new horario  y vola :)
    yo arreglo lo que me pitie <3

---
+++ v1/appSIA/utemAppWF/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace utemAppWF
+{
+    public class Class1
+    {
+        string ConnStr = @"Data Source = C:\sisLabUtem.db ; Version = 3";
+
+        public void GetData(string Query, DataGrid dataGrid)
+        {
+            using (SQLiteConnection Conn = new SQLiteConnection(ConnStr))
+                try
+                {
+
+                    SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(Query, Conn);
+                    Conn.Open();
+                    DataTable table = new DataTable();
+                    table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                    dataAdapter.Fill(table);
+                    dataGrid.DataSource = table;
+                    Conn.Close();
+
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Problema con la BDD");
+                }
+        }
+
+    }
+}
---
+++ v1/appSIA/utemAppWF/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace utemAppWF
+{
+    /*Clase que sirve para ayudar a conectar la base de datos a la app*/ 
+
+    public static class Helper
+    {   
+        /*
+        public static string CnnStr(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+        }*/
+    }
+}
